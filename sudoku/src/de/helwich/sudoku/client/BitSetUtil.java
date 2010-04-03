9a6a2947815442f5d163cf646b3c63bf02d0eddd
hhelwich <hhelwich@82ca4850-2ef3-cc52-f967-50850b2f339d>
2010-04-03 14:00:12 +0000

    add class and related test
    git-svn-id: https://web-sudoku.googlecode.com/svn/trunk@5 82ca4850-2ef3-cc52-f967-50850b2f339d

---
+++ sudoku/src/de/helwich/sudoku/client/BitSetUtil.java
@@ -0,0 +1,152 @@
+package de.helwich.sudoku.client;
+
+
+/**
+ * Static operations to operate with int bitsets (32 bit).
+ * 
+ * @author Hendrik Helwich
+ */
+public class BitSetUtil {
+	
+	private BitSetUtil() {}
+
+    /** maximum index of a bitset (minimum index is 0) */
+    public static final int MAX_INDEX = 31;
+    
+	/** int with all bits set */
+	private static final int INT_MASK = 0xffffffff;
+
+	/**
+     * Sets the bit at the specified index to <code>true</code>.
+     *
+     * @param  bitset
+     *         a bitset.
+     * @param  bitIndex
+     *         a bit index.
+     * @return the bitset with the set bit.
+     * @throws IndexOutOfBoundsException
+     *         if the specified index is negative or greater than
+     *         {@link #MAX_INDEX}
+     */
+    public static int set(int bitset, int bitIndex)
+    		throws IndexOutOfBoundsException {
+		checkIndexRange(bitIndex, true);
+		return bitset | (1 << bitIndex);
+    }
+
+    /**
+     * Sets the bit specified by the index to <code>false</code>.
+     *
+     * @param  bitset
+     *         a bitset.
+     * @param  bitIndex
+     *         the index of the bit to be cleared.
+     * @return the bitset with the cleared bit.
+     * @throws IndexOutOfBoundsException
+     *         if the specified index is negative or greater than
+     *         {@link #MAX_INDEX}
+     */
+    public static int clear(int bitset, int bitIndex)
+			throws IndexOutOfBoundsException {
+		checkIndexRange(bitIndex, true);
+		return bitset & ~(1 << bitIndex);
+    }
+    
+    /**
+     * Returns the value of the bit with the specified index. The value
+     * is <code>true</code> if the bit with the index <code>bitIndex</code>
+     * is currently set in this <code>BitSet</code>; otherwise, the result
+     * is <code>false</code>.
+     *
+     * @param  bitset
+     *         a bitset.
+     * @param  bitIndex
+     *         the bit index.
+     * @return the value of the bit with the specified index.
+     * @throws IndexOutOfBoundsException
+     *         if the specified index is negative or greater than
+     *         {@link #MAX_INDEX}
+     */
+    public static boolean get(int bitset, int bitIndex)
+			throws IndexOutOfBoundsException {
+		checkIndexRange(bitIndex, true);
+    	return (bitset & (1 << bitIndex)) != 0;
+    }
+	
+	/**
+	 * Returns the index of the first bit that is set to <code>true</code>
+	 * that occurs on or after the specified starting index. If no such
+	 * bit exists then -1 is returned.
+	 * 
+     * To iterate over the <code>true</code> bits in a bitset,
+     * use the following loop:
+     * 
+	 * <pre>for (int i = nextSetBit(bitset, 0); i >= 0; i = nextSetBit(bitset, i+1))
+	 * 		; // do something</pre>
+	 * 
+	 * @param  bitset
+     *         a bitset.
+	 * @param  fromIndex
+	 *         the bit index after which the first bit that is set to 
+	 *         <code>true</code> is returned.
+	 * @return the index of the first bit that is set to <code>true</code>
+	 *         that occurs on or after the specified starting index
+	 * @throws IndexOutOfBoundsException
+     *         if the specified index is negative
+	 */
+	public static int nextSetBit(int bitset, int fromIndex)
+			throws IndexOutOfBoundsException {
+		checkIndexRange(fromIndex, false);
+		if (fromIndex > MAX_INDEX)
+			return -1;
+		bitset &= (INT_MASK << fromIndex);
+	    if (bitset != 0)
+			return Integer.numberOfTrailingZeros(bitset);
+		return -1;
+	}
+	
+	/**
+	 * Throws an {@link IndexOutOfBoundsException} if the given bit index is
+	 * negative or if it the second argument is <code>true</code> also if the
+	 * index is greater than {@link #MAX_INDEX}.
+	 * 
+	 * @param  index
+	 * @param  checkMax
+	 * @throws IndexOutOfBoundsException
+	 */
+	private static void checkIndexRange(int index, boolean checkMax)
+			throws IndexOutOfBoundsException {
+    	if (index < 0 || (checkMax && index > MAX_INDEX))
+    		throw new IndexOutOfBoundsException("bitIndex [0..."+MAX_INDEX+"]: " + index);
+	}
+	
+	/**
+	 * For the given list of bitsets return all possible permutations of the bit
+	 * indices.
+	 * 
+	 * @param  bitsets
+	 * @return
+	 */
+	public static int[] getPossiblePermutations(int[] bitsets) {
+		int[] nsets = new int[bitsets.length];
+		getPossiblePermutations(bitsets, nsets, 0, 0);
+		return nsets;
+	}
+	
+	private static boolean getPossiblePermutations(int[] bitsets, int[] newbitsets, int bitsetIndex, int usedBits) {
+		//TODO optimize
+		int bitset = bitsets[bitsetIndex];
+		bitset = bitset & (~ usedBits);
+		if (bitset == 0)
+			return false;
+		if (bitsetIndex + 1 == bitsets.length)
+			newbitsets[bitsetIndex] |= bitset;
+		else for (int i = nextSetBit(bitset, 0); i >= 0; i = nextSetBit(bitset, i+1)) {
+			int used = set(usedBits, i);
+			if (getPossiblePermutations(bitsets, newbitsets, bitsetIndex + 1, used))
+				newbitsets[bitsetIndex] = set(newbitsets[bitsetIndex], i);
+		}
+		return newbitsets[bitsetIndex] != 0;
+	}
+	
+}
---
+++ sudoku/test/de/helwich/sudoku/client/BitSetUtilTest.java
@@ -0,0 +1,86 @@
+package de.helwich.sudoku.client;
+
+import static de.helwich.sudoku.client.BitSetUtil.*;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Hendrik Helwich
+ */
+public class BitSetUtilTest {
+
+	int bitset;
+	
+	@Before
+	public void setUp() throws Exception {
+		bitset = 0; // empty set
+		bitset = set(bitset, 3);
+		bitset = set(bitset, 7);
+		bitset = set(bitset, 2);
+		bitset = set(bitset, MAX_INDEX);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testNextSetBit() {
+		int set2 = 0;
+		for (int i = nextSetBit(bitset, 0); i >= 0; i = nextSetBit(bitset, i+1))
+			set2 = set(set2, i);
+		assertEquals(bitset, set2);
+	}
+
+	@Test
+	public void testMaxIndex() {
+		assertEquals(31, MAX_INDEX);
+	}
+
+	@Test
+	public void testSetAndClearAndGet() {
+		for (int i = 0; i <= MAX_INDEX; i ++)
+			assertEquals(
+					i == 2 || i == 3 || i == 7 || i == MAX_INDEX, 
+					get(bitset, i));
+		bitset = clear(bitset, 3);
+		bitset = clear(bitset, 5);
+		bitset = clear(bitset, MAX_INDEX);
+		for (int i = 0; i <= MAX_INDEX; i ++)
+			assertEquals(
+					i == 2 || i == 7, 
+					get(bitset, i));
+	}
+
+	@Test
+	public void testGetPossiblePermutations() {
+		int count = 6;
+		int[] bitsets = new int[count];
+		for (int i = 0; i < count; i ++)
+			bitsets[0] = set(bitsets[0], i);
+		for (int i = 1; i < count; i ++)
+			bitsets[i] = bitsets[0];
+		int[] newbs = getPossiblePermutations(bitsets);
+		for (int i = 0; i < count; i ++)
+			assertEquals(bitsets[0], newbs[i]);
+	}
+
+	@Test
+	public void testGetPossiblePermutations2() {
+		int[] bitsets = new int[3];
+		bitsets[0] = set(bitsets[0], 1);
+		bitsets[0] = set(bitsets[0], 3);
+		bitsets[1] = set(bitsets[1], 1);
+		bitsets[1] = set(bitsets[1], 2);
+		bitsets[1] = set(bitsets[1], 3);
+		bitsets[2] = bitsets[0];
+		int[] newbs = getPossiblePermutations(bitsets);
+		assertEquals(bitsets[0], newbs[0]);
+		assertEquals(set(0, 2), newbs[1]);
+		assertEquals(bitsets[0], newbs[2]);
+	}
+
+}
