1b9111eeb030e9ad0b159c5ee1861110776ee9c5
hhelwich <hhelwich@82ca4850-2ef3-cc52-f967-50850b2f339d>
2010-04-14 20:37:30 +0000

    * added getPossiblePermutationsFast(int[]) * added set(int, int, int)
    git-svn-id: https://web-sudoku.googlecode.com/svn/trunk@22 82ca4850-2ef3-cc52-f967-50850b2f339d

--- sudoku/src/de/helwich/sudoku/client/BitSetUtil.java
+++ sudoku/src/de/helwich/sudoku/client/BitSetUtil.java
@@ -1,5 +1,9 @@
 package de.helwich.sudoku.client;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
 /**
  * Static operations to operate with int bitsets (32 bit).
  * 
@@ -7,8 +11,7 @@ package de.helwich.sudoku.client;
  */
 public class BitSetUtil {
 
-	private BitSetUtil() {
-	}
+	private BitSetUtil() {}
 
 	/** maximum index of a bitset (minimum index is 0) */
 	public static final int MAX_INDEX = 31;
@@ -30,9 +33,36 @@ public class BitSetUtil {
 	 */
 	public static int set(int bitset, int bitIndex)
 			throws IndexOutOfBoundsException {
-		checkIndexRange(bitIndex, true);
+		checkIndexRange(bitIndex, MAX_INDEX);
 		return bitset | (1 << bitIndex);
 	}
+
+    /**
+     * Sets the bits from the specified <tt>fromIndex</tt> (inclusive) to the
+     * specified <tt>toIndex</tt> (exclusive) to <code>true</code>.
+     * 
+     * @param  bitset
+     * @param  fromIndex
+     *         index of the first bit to be set.
+     * @param  toIndex
+     *         index after the last bit to be set.
+     * @return
+     * @throws IndexOutOfBoundsException
+     *         if <tt>fromIndex</tt> is negative,
+     *         or <tt>toIndex</tt> is negative, or <tt>fromIndex</tt> is
+     *         larger than <tt>toIndex</tt>.
+     * @throws IllegalArgumentException
+     */
+    public static int set(int bitset, int fromIndex, int toIndex)
+			throws IndexOutOfBoundsException, IllegalArgumentException {
+		checkIndexRange(fromIndex, MAX_INDEX);
+		checkIndexRange(toIndex, MAX_INDEX+1);
+		if (fromIndex > toIndex)
+			throw new IllegalArgumentException("fromIndex parameter must be lower or equal to toIndex parameter");
+		int mask = INT_MASK << fromIndex;
+		mask &= INT_MASK >>> -toIndex;
+		return bitset | mask;
+    }
 
 	/**
 	 * Sets the bit specified by the index to <code>false</code>.
@@ -48,7 +78,7 @@ public class BitSetUtil {
 	 */
 	public static int clear(int bitset, int bitIndex)
 			throws IndexOutOfBoundsException {
-		checkIndexRange(bitIndex, true);
+		checkIndexRange(bitIndex, MAX_INDEX);
 		return bitset & ~(1 << bitIndex);
 	}
 
@@ -69,7 +99,7 @@ public class BitSetUtil {
 	 */
 	public static boolean get(int bitset, int bitIndex)
 			throws IndexOutOfBoundsException {
-		checkIndexRange(bitIndex, true);
+		checkIndexRange(bitIndex, MAX_INDEX);
 		return (bitset & (1 << bitIndex)) != 0;
 	}
 
@@ -98,7 +128,7 @@ public class BitSetUtil {
 	 */
 	public static int nextSetBit(int bitset, int fromIndex)
 			throws IndexOutOfBoundsException {
-		checkIndexRange(fromIndex, false);
+		checkIndexRange(fromIndex, Integer.MAX_VALUE);
 		if (fromIndex > MAX_INDEX)
 			return -1;
 		bitset &= (INT_MASK << fromIndex);
@@ -116,9 +146,9 @@ public class BitSetUtil {
 	 * @param  checkMax
 	 * @throws IndexOutOfBoundsException
 	 */
-	private static void checkIndexRange(int index, boolean checkMax)
+	private static void checkIndexRange(int index, int maxIndex)
 			throws IndexOutOfBoundsException {
-		if (index < 0 || (checkMax && index > MAX_INDEX))
+		if (index < 0 || index > maxIndex)
 			throw new IndexOutOfBoundsException("bitIndex [0..." + MAX_INDEX
 					+ "]: " + index);
 	}
@@ -145,7 +175,62 @@ public class BitSetUtil {
 	public static boolean subset(int subbitset, int bitset) {
 		return (bitset & subbitset) == subbitset;
 	}
-
+	
+	/**
+	 * Faster try of {@link #getPossiblePermutations(int[])}
+	 * 
+	 * @param  bitsets
+	 */
+	public static void getPossiblePermutationsFast(int[] bitsets) {
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int i = 0 ; i < bitsets.length; i++) {
+			if (bitsets[i] == 0) {
+				for (i = 0 ; i < bitsets.length; i++)
+					bitsets[i] = 0;
+				return;
+			}
+			for (int key : map.keySet())
+				if (subset(bitsets[i], key))
+					map.put(key, map.get(key)+1);
+			Integer set = map.get(bitsets[i]);
+			if (set == null) {
+				int cnt = 1;
+				for (int j = 0 ; j < i; j++) 
+					if (subset(bitsets[j], bitsets[i]))
+						cnt ++;
+				map.put(bitsets[i], cnt);
+			}
+			for (int key : new HashSet<Integer>(map.keySet())) {
+				int union = bitsets[i] | key;
+				if (!map.containsKey(union)) {
+					int count = 0;
+					for (int j = 0 ; j <= i; j++) 
+						if (subset(bitsets[j], union))
+							count ++;
+					map.put(union, count);
+				}
+			}
+		}
+		for (int s : map.keySet()) {
+			int v = map.get(s);
+			if (cardinality(s) == v) {
+				boolean change = false;
+				for (int i = 0 ; i < bitsets.length; i++)
+					if (!subset(bitsets[i], s)) {
+						int n = bitsets[i] & (~s);
+						change |= n != bitsets[i];
+						bitsets[i] = n;
+					}
+				if (change) {
+					getPossiblePermutationsFast(bitsets);
+					return;
+				}
+			} else if (cardinality(s) < v)
+				for (int i = 0 ; i < bitsets.length; i++)
+					bitsets[i] = 0;
+		}
+	}
+	
 	/**
 	 * For the given list of bitsets return all possible permutations of the bit
 	 * indices.
