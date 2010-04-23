4e503fd1167a5828b5295fdab25c4e5185c5128a
hhelwich <hhelwich@82ca4850-2ef3-cc52-f967-50850b2f339d>
2010-04-23 12:07:47 +0000

    added operations * isSolved() * hasNull() * toString()
    git-svn-id: https://web-sudoku.googlecode.com/svn/trunk@25 82ca4850-2ef3-cc52-f967-50850b2f339d

--- sudoku/src/de/helwich/sudoku/client/Field.java
+++ sudoku/src/de/helwich/sudoku/client/Field.java
@@ -1,5 +1,7 @@
 package de.helwich.sudoku.client;
 
+import static de.helwich.sudoku.client.BitSetUtil.*;
+
 import java.util.LinkedList;
 import java.util.List;
 
@@ -18,6 +20,8 @@ public class Field {
 	private final Type type;
 	private int[][] field;
 	private List<CellChangeHandler> changeHandlers;
+	private int countNull; // number of cells with a zero value
+	private int countUnique; // number of cells with a value with cardinality one
 	
 	//TODO maybe optimize class eg by not needing type copy
 	/**
@@ -28,6 +32,8 @@ public class Field {
 	public Field(Type type) {
 		this.type = type.copy(); // copy because type can change
 		field = new int[this.type.getHeight()][this.type.getWidth()];
+		countNull = type.getCellCount();
+		countUnique = 0;
 	}
 
 	/**
@@ -45,11 +51,47 @@ public class Field {
 			throw new IndexOutOfBoundsException("cell index "+row+","+column
 					+" does not exist");
 		if (field[row][column] != bitset) {
+			// update countNull parameter
+			if (bitset == 0) {
+				if (field[row][column] != 0)
+					countNull ++;
+			} else if (field[row][column] == 0)
+				countNull --;
+			// update countUnique parameter
+			if (cardinality(bitset) == 1) {
+				if (cardinality(field[row][column]) != 1)
+					countUnique ++;
+			} else if (cardinality(field[row][column]) == 1)
+				countUnique --;
+			// store value
 			field[row][column] = bitset;
+			// notify change listeners
 			if (changeHandlers != null)
 				for (CellChangeHandler handler : changeHandlers)
 					handler.onChange(row, column, bitset);
 		}
+	}
+	
+	/**
+	 * Returns <code>true</code> if the operation {@link #getBitset(int, int)}
+	 * will return a value with cardinality 1 for all valid field cell indices.
+	 * 
+	 * @return <code>true</code> if the operation {@link #getBitset(int, int)}
+	 * will return a value with cardinality 1 for all valid field cell indices.
+	 */
+	public boolean isSolved() {
+		return countUnique == type.getCellCount();
+	}
+	
+	/**
+	 * Returns <code>true</code> if the operation {@link #getBitset(int, int)}
+	 * will return a zero value for at least one valid field cell index.
+	 * 
+	 * @return <code>true</code> if the operation {@link #getBitset(int, int)}
+	 * will return a zero value for at least one valid field cell index.
+	 */
+	public boolean hasNull() {
+		return countNull > 0;
 	}
 	
 	/**
@@ -83,6 +125,36 @@ public class Field {
 
 	public Type getType() {
 		return type;
+	}
+
+	/*
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < type.getWidth()*2+3; i++)
+			sb.append("-");
+		sb.append("\n");
+		for (int i = 0; i < type.getHeight(); i++) {
+			sb.append("|");
+			for (int j = 0; j < type.getWidth(); j++) {
+				if (type.hasCellIndex(i, j)) {
+					int set = getBitset(i, j);
+					if (cardinality(set) == 1)
+						sb.append(" "+type.getFieldChars().charAt(nextSetBit(set, 0)));
+					else if (set == 0)
+						sb.append(" #");
+					else
+						sb.append(" .");
+				} else
+					sb.append("  ");
+			}
+			sb.append(" |\n");
+		}
+		for (int i = 0; i < type.getWidth()*2+3; i++)
+			sb.append("-");
+		return sb.toString();
 	}
 	
 }
