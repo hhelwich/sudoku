e61ed98c220e1fdeda9519baae8f225a3cfd0e46
hhelwich <hhelwich@82ca4850-2ef3-cc52-f967-50850b2f339d>
2010-05-23 21:15:18 +0000

    add exception class
    git-svn-id: https://web-sudoku.googlecode.com/svn/trunk@60 82ca4850-2ef3-cc52-f967-50850b2f339d

---
+++ sudoku/src/de/helwich/sudoku/client/NotSolvableException.java
@@ -0,0 +1,12 @@
+package de.helwich.sudoku.client;
+
+/**
+ * @author Hendrik Helwich
+ */
+public class NotSolvableException extends Exception {
+
+	private static final long serialVersionUID = 9013427916138529191L;
+
+	public NotSolvableException() {}
+
+}
