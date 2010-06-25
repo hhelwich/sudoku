5225d987af44c12752a4ded0c5dfb871af0e1b90
hhelwich <hhelwich@82ca4850-2ef3-cc52-f967-50850b2f339d>
2010-06-25 21:02:38 +0000

    added possibility to restore removed nodes
    git-svn-id: https://web-sudoku.googlecode.com/svn/trunk@70 82ca4850-2ef3-cc52-f967-50850b2f339d

--- sudoku/src/de/helwich/sudoku/client/FieldSolveMatrix.java
+++ sudoku/src/de/helwich/sudoku/client/FieldSolveMatrix.java
@@ -1,16 +1,18 @@
 package de.helwich.sudoku.client;
 
+import java.util.ArrayList;
 import java.util.BitSet;
+import java.util.List;
 
 /**
  * @author Hendrik Helwich
  */
 public class FieldSolveMatrix {
 
-	int cellCount;
-	int valueCount;
-	int groupCount;
-	Node[] nodes;
+	private int cellCount;
+	private int valueCount;
+	private int groupCount;
+	private Node[] nodes;
 	
 	public FieldSolveMatrix(Type2 type) {
 		cellCount = type.getCellCount();
@@ -44,63 +46,81 @@ public class FieldSolveMatrix {
 			nodes[c] = nodes[c].first();
 	}
 	
-}
+	public void restoreNodes() {
+		if (!STORE_REMOVED_NODES)
+			throw new RuntimeException();
+		while (!removedNodes.isEmpty()) {
+			Node lastRemoved = removedNodes.remove(removedNodes.size()-1);
+			lastRemoved.reInsert();
+		}
+	}
 
-class Node {
-	
-	public Node left, right, up, down;
-	public final int hIndex;
-	
-	public Node(int hIndex) {
-		this.hIndex = hIndex;
-	}
-	
-	public Node first() {
-		Node node = this;
-		while (node.left != null)
-			node = node.left;
-		return node;
-	}
-	
-	public void remove() {
-		if (up != null)
-			up.down = down;
-		if (down != null)
-			down.up = up;
-		if (left != null)
-			left.right = right;
-		if (right != null)
-			right.left = left;
-	}
-	
-	public void reInsert() {
-		if (up != null)
-			up.down = this;
-		if (down != null)
-			down.up = this;
-		if (left != null)
-			left.right = this;
-		if (right != null)
-			right.left = this;
-	}
-	
-	public void clear() {
-		up = null;
-		down = null;
-		left = null;
-		right = null;
-	}
-	
-	public boolean isRemoved() {
-		if (up != null)
-			return up.down != this;
-		if (down != null)
-			return down.up != this;
-		if (left != null)
-			return left.right != this;
-		if (right != null)
-			return right.left != this;
-		return true;
+	private boolean STORE_REMOVED_NODES = true; 
+	private List<Node> removedNodes = STORE_REMOVED_NODES ? new ArrayList<Node>() : null;
+
+	class Node {
+		
+		public Node left, right, up, down;
+		public final int hIndex;
+		
+		public Node(int hIndex) {
+			this.hIndex = hIndex;
+		}
+		
+		public Node first() {
+			Node node = this;
+			while (node.left != null)
+				node = node.left;
+			return node;
+		}
+		
+		public void remove() {
+			if (!isRemoved()) {
+				if (up != null)
+					up.down = down;
+				if (down != null)
+					down.up = up;
+				if (left != null)
+					left.right = right;
+				if (right != null)
+					right.left = left;
+				if (STORE_REMOVED_NODES)
+					removedNodes.add(this);
+//				else
+//					clear(); // clear to be removed by garbage collector
+			}
+		}
+		
+		public void reInsert() {
+			if (up != null)
+				up.down = this;
+			if (down != null)
+				down.up = this;
+			if (left != null)
+				left.right = this;
+			if (right != null)
+				right.left = this;
+		}
+		
+//		private void clear() {
+//			up = null;
+//			down = null;
+//			left = null;
+//			right = null;
+//		}
+		
+		public boolean isRemoved() {
+			if (up != null)
+				return up.down != this;
+			if (down != null)
+				return down.up != this;
+			if (left != null)
+				return left.right != this;
+			if (right != null)
+				return right.left != this;
+			return true;
+		}
+		
 	}
 	
 }
